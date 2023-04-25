27078b6367c9021c6778aa70c44c8d998c8ab852
Nowy <90156768+Nowyme@users.noreply.github.com>
2023-04-25 22:26:46 +0200

    fix sheep

---
+++ Assets/Scripts/MoveFromPlayer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFromPlayer : MonoBehaviour
+{
+public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rb.rotation = angle;
+        direction.Normalize();
+        movement = -direction;
+    }
+
+ 
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("renne: " + movement);
+            // rb.MovePosition((Vector2)transform.position + (movement * moveSpeed * Time.deltaTime));
+            if (Mathf.Abs (rb.velocity.x) >= moveSpeed) {								//If sideways velocity is higher than or equal to set movement speed,
+			    rb.velocity = new Vector2 (1 * moveSpeed, rb.velocity.y);	// set sideways velocity to movement speed.
+            } else {
+                rb.AddForce (new Vector2 (1 * (moveSpeed * 10f), 0));		//Else add sideways force to player
+            }
+        }
+    }
+
+//     void OnTriggerExit2D(Collider2D other){
+//          if (other.CompareTag("Player"))
+//         {
+//             rb.velocity = Vector2.zero;
+//         }
+//     }
+}
---
+++ Assets/Scripts/SheepController.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour
+{
+    public float normalSpeed = 1.0f;
+    public float scaredSpeed = 2.0f;
+    public float detectionRadius = 5.0f;
+    public string playerTag = "Player";
+    public float stopChance = 0.1f;
+
+    private GameObject player;
+    private Rigidbody2D rb;
+    private Vector2 randomDirection;
+    private float directionChangeInterval = 2.0f;
+
+    // Animation State
+    Animator animator;
+    string currentState;
+
+    const string SHEEP_IDLE = "Sheep_Idle";
+    const string SHEEP_WALK_FRONT = "Sheep_Front";
+    const string SHEEP_WALK_BACK = "Sheep_Back";
+    const string SHEEP_WALK_LEFT = "Sheep_Left";
+    const string SHEEP_WALK_RIGHT = "Sheep_Right";
+    const string SHEEP_IDLE_FRONT = "Sheep_Idle_Front";
+    const string SHEEP_IDLE_BACK = "Sheep_Idle_Back";
+    const string SHEEP_IDLE_LEFT = "Sheep_Idle_Left";
+    const string SHEEP_IDLE_RIGHT = "Sheep_Idle_Right";
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag(playerTag);
+        animator = GetComponent<Animator>();
+        StartCoroutine(ChangeDirection());
+    }
+
+    void Update()
+{
+    float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+    if (distanceToPlayer < detectionRadius)
+    {
+        Vector2 fleeDirection = (transform.position - player.transform.position).normalized;
+
+        // Only move in straight lines
+        float absX = Mathf.Abs(fleeDirection.x);
+        float absY = Mathf.Abs(fleeDirection.y);
+        if (absX > absY)
+        {
+            fleeDirection = new Vector2(Mathf.Sign(fleeDirection.x), 0);
+        }
+        else
+        {
+            fleeDirection = new Vector2(0, Mathf.Sign(fleeDirection.y));
+        }
+
+        rb.velocity = fleeDirection * scaredSpeed;
+    }
+    else
+    {
+        rb.velocity = randomDirection;
+    }
+
+    UpdateAnimation();
+}
+
+    IEnumerator ChangeDirection()
+    {
+        while (true)
+        {
+            if (Random.value < stopChance)
+            {
+                randomDirection = Vector2.zero;
+            }
+            else
+            {
+                float moveX = (Random.value < 0.5f) ? -1 : 1;
+                float moveY = (Random.value < 0.5f) ? -1 : 1;
+
+                if (Random.value < 0.5f)
+                {
+                    randomDirection = new Vector2(moveX * normalSpeed, 0);
+                }
+                else
+                {
+                    randomDirection = new Vector2(0, moveY * normalSpeed);
+                }
+            }
+            yield return new WaitForSeconds(directionChangeInterval);
+        }
+    }
+
+    void UpdateAnimation()
+{
+    if (rb.velocity.x > 0)
+    {
+        ChangeAnimationState(SHEEP_WALK_RIGHT);
+        if (Mathf.Approximately(rb.velocity.x, 0f))
+        {
+            ChangeAnimationState(SHEEP_IDLE_RIGHT);
+        }
+    }
+    else if (rb.velocity.x < 0)
+    {
+        ChangeAnimationState(SHEEP_WALK_LEFT);
+        if (Mathf.Approximately(rb.velocity.x, 0f))
+        {
+            ChangeAnimationState(SHEEP_IDLE_LEFT);
+        }
+    }
+    else if (rb.velocity.y > 0)
+    {
+        ChangeAnimationState(SHEEP_WALK_BACK);
+        if (Mathf.Approximately(rb.velocity.y, 0f))
+        {
+            ChangeAnimationState(SHEEP_IDLE_BACK);
+        }
+    }
+    else if (rb.velocity.y < 0)
+    {
+        ChangeAnimationState(SHEEP_WALK_FRONT);
+        if (Mathf.Approximately(rb.velocity.y, 0f))
+        {
+            ChangeAnimationState(SHEEP_IDLE_FRONT);
+        }
+    }
+    else
+    {
+        if (currentState == SHEEP_WALK_FRONT)
+        {
+            ChangeAnimationState(SHEEP_IDLE_FRONT);
+        }
+        else if (currentState == SHEEP_WALK_BACK)
+        {
+            ChangeAnimationState(SHEEP_IDLE_BACK);
+        }
+        else if (currentState == SHEEP_WALK_LEFT)
+        {
+            ChangeAnimationState(SHEEP_IDLE_LEFT);
+        }
+        else if (currentState == SHEEP_WALK_RIGHT)
+        {
+            ChangeAnimationState(SHEEP_IDLE_RIGHT);
+        }
+    }
+}
+
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+
+        currentState = newState;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+            // Player caught the sheep, perform desired actions here
+            Debug.Log("Player caught the sheep!");
+        }
+    }
+}
