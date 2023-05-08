9a9deebefa9272a60d3a6b74d55ef14c86cfa986
Nowy <90156768+Nowyme@users.noreply.github.com>
2023-05-08 22:10:05 +0200

    world

---
+++ Assets/StartAnimationAtRandomFRame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimationAtRandomFRame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var anim = GetComponent<Animator>();
+        var state = anim.GetCurrentAnimatorStateInfo(0);
+        anim.Play(state.fullPathHash, 0, Random.Range(0f, 1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
