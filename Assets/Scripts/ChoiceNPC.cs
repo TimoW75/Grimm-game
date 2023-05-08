31eb79f0c124252b7334971a32e958737d7955ae
Timo werkhoven <timo2003werkhoven@gmail.com>
2023-05-08 14:41:38 +0200

    Add npc

--- Assets/Scripts/ChoiceNPC.cs
+++ Assets/Scripts/ChoiceNPC.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ChoiceNPC : MonoBehaviour
 {
@@ -8,7 +9,44 @@ public class ChoiceNPC : MonoBehaviour
     [SerializeField] private GameObject[] Field;
     [SerializeField] private string[] rightWords;
     public InventoryManager inventoryM;
-    private bool isClose;
+
+
+    [Header("NPC Dialogue Box")]
+    [SerializeField] private Sprite npcIcon;
+    [SerializeField] private string NPCname;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI textComponent;
+
+    [SerializeField] private Image npcImageObject;
+    [SerializeField] private TextMeshProUGUI npcName;
+
+    [SerializeField] private string[] lines;
+    [SerializeField] private string questActiveText = "You are already doing my quest!";
+    [SerializeField] private string[] HasQuestItemText;
+
+    [SerializeField] private float textSpeed;
+
+    private int index;
+    private bool playerIsClose;
+
+    [Header("NPC Quest")]
+    [SerializeField] private bool hasQuest;
+    [SerializeField] private string ItemNeededForQuest;
+    [SerializeField] private string ItemNeededInInvToCompleteQuestName;
+    [SerializeField] private Item givenQuestItem;
+    private bool questActive;
+    private bool questCompeleted;
+    private bool itemReceived;
+    private bool hasReceivedClue;
+
+    public InventoryManager inventoryManage;
+
+    public PlayerChopping playerChop;
+    [SerializeField] private GameObject SubmitQuestItemSlot;
+
+    [SerializeField] private int QuestActiveOnDay;
+    public gameManager gameManager;
+
 
     void Start()
     {
@@ -16,33 +54,92 @@ public class ChoiceNPC : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && isClose)
+        if(gameManager.dayNumber == 3 && !questCompeleted)
         {
-            if (choicePanel.activeInHierarchy)
+            if (Input.GetKeyDown(KeyCode.F) && playerIsClose)
             {
-                choicePanel.SetActive(false);
-                inventory.SetActive(false);
-                DestroyImageChildrenWithTag();
+                if (dialoguePanel.activeInHierarchy)
+                {
+                    zeroText();
+                }
+                else
+                {
+                    dialoguePanel.SetActive(true);
+                    StartDialgue();
+
+                }
             }
-            else
+            if (Input.GetKeyDown(KeyCode.Return) && playerIsClose && !questActive && !hasReceivedClue)
             {
-                choicePanel.SetActive(true);
-                inventory.SetActive(true);
+                if (dialoguePanel.activeInHierarchy)
+                {
+                    NextLine();
+                }
             }
+            else if (Input.GetKeyDown(KeyCode.Return) && playerIsClose && !questActive && hasReceivedClue)
+            {
+                if (dialoguePanel.activeInHierarchy)
+                {
+                    zeroText();
+                    hasReceivedClue = false;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.Return) && playerIsClose && questActive && !hasReceivedClue && !questCompeleted)
+            {
+                if (dialoguePanel.activeInHierarchy)
+                {
+                    NextLine();
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.Return) && playerIsClose && questActive && hasReceivedClue && questCompeleted)
+            {
+                if (dialoguePanel.activeInHierarchy)
+                {
+                    NextLine();
+                }
+            }
+
         }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.F) && playerIsClose)
+            {
+                if (choicePanel.activeInHierarchy)
+                {
+                    choicePanel.SetActive(false);
+                    inventory.SetActive(false);
+                    DestroyImageChildrenWithTag();
+                }
+                else
+                {
+                    choicePanel.SetActive(true);
+                    inventory.SetActive(true);
+                }
+            }
+        }     
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        isClose = true;
+        if (collision.CompareTag("Player"))
+        {
+            playerIsClose = true;
+
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        isClose = false;
         choicePanel.SetActive(false);
         inventory.SetActive(false);
         checkFilledFields();
         DestroyImageChildrenWithTag();
+
+        if (collision.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            dialoguePanel.SetActive(false);
+            zeroText();
+        }
     }
 
     void DestroyImageChildrenWithTag()
@@ -76,4 +173,167 @@ public class ChoiceNPC : MonoBehaviour
             }
         }
     }
+
+
+    public void zeroText()
+    {
+        textComponent.text = string.Empty;
+        index = 0;
+        dialoguePanel.SetActive(false);
+        if (SubmitQuestItemSlot != null)
+        {
+            SubmitQuestItemSlot.SetActive(false);
+        }
+        if (inventory != null)
+        {
+            inventory.SetActive(false);
+        }
+    }
+
+    void StartDialgue()
+    {
+        npcImageObject.sprite = npcIcon;
+        npcName.text = NPCname;
+        index = 0;
+
+        if (QuestActiveOnDay == gameManager.dayNumber)
+        {
+            if (gameManager.questActiveGeneral && !questActive)
+            {
+                textComponent.text = "You are already doing a quest";
+            }
+            else if (!questActive && !questCompeleted && !gameManager.questActiveGeneral)
+            {
+                textComponent.text = lines[index];
+
+            }
+            else if (questActive && !questCompeleted)
+            {
+                if (inventory != null && SubmitQuestItemSlot != null)
+                {
+                    inventory.SetActive(true);
+                    inventoryManage.ListItems();
+                }
+                if (SubmitQuestItemSlot != null)
+                {
+                    SubmitQuestItemSlot.SetActive(true);
+                    if (SubmitQuestItemSlot.transform.GetChild(0).transform.childCount != 0)
+                    {
+                        GameObject.Destroy(SubmitQuestItemSlot.transform.GetChild(0).transform.GetChild(0).gameObject);
+                    }
+                    textComponent.text = HasQuestItemText[index];
+                }
+                else
+                {
+                    textComponent.text = questActiveText;
+                }
+
+            }
+            else if (questCompeleted && questActive && hasReceivedClue)
+            {
+                textComponent.text = "You have already completed my quest!";
+            }
+        }
+        else
+        {
+            textComponent.text = "Come back tomorrow";
+        }
+
+
+
+    }
+
+
+    void NextLine()
+    {
+        if (hasQuest && QuestActiveOnDay == gameManager.dayNumber)
+        {
+
+            if (index < lines.Length - 1 && !questActive && !gameManager.questActiveGeneral)
+            {
+                index++;
+                textComponent.text = string.Empty;
+                textComponent.text = lines[index];
+            }
+            else if (questCompeleted && questActive && itemReceived && hasReceivedClue)
+            {
+                zeroText();
+            }
+            else if (!questActive && !questCompeleted)
+            {
+                questActive = true;
+                gameManager.questActiveGeneral = true;
+                gameManager.currentQuest = gameObject.name;
+                gameManager.setTextActive();
+
+                if (ItemNeededForQuest == "Axe" && playerChop != null)
+                {
+                    playerChop.setActiveAxe();
+                }
+                zeroText();
+
+            }
+            else if (questActive && !questCompeleted)
+            {
+                if (SubmitQuestItemSlot.transform.GetChild(0).transform.childCount != 0)
+                {
+                    if (SubmitQuestItemSlot.transform.GetChild(0).transform.GetChild(0).name == ItemNeededInInvToCompleteQuestName)
+                    {
+                        if (index < HasQuestItemText.Length - 1)
+                        {
+                            index++;
+                            textComponent.text = string.Empty;
+                            textComponent.text = HasQuestItemText[index];
+                        }
+                        else if (!hasReceivedClue)
+                        {
+
+                            if (!itemReceived)
+                            {
+                                InventoryManager.Instance.AddItem(givenQuestItem);
+                                itemReceived = true;
+                                questCompeleted = true;
+                            }
+                            else
+                            {
+                                zeroText();
+                            }
+                            hasReceivedClue = true;
+                            gameManager.questActiveGeneral = false;
+                            gameManager.currentQuest = string.Empty;
+                            gameManager.setTextHiden();
+
+                        }
+                        else
+                        {
+                            zeroText();
+                        }
+                    }
+                    else
+                    {
+                        textComponent.text = "I have not received the item I asked for yet";
+                    }
+
+                }
+            }
+        }
+        else if (!hasQuest && !gameManager.questActiveGeneral && QuestActiveOnDay == gameManager.dayNumber)
+        {
+            if (index < lines.Length - 1)
+            {
+                index++;
+                textComponent.text = string.Empty;
+                textComponent.text = lines[index];
+            }
+            else
+            {
+                zeroText();
+            }
+        }
+        else if (QuestActiveOnDay != gameManager.dayNumber)
+        {
+            zeroText();
+        }
+    }
+
 }
