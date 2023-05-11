75a2e78a36b5982f61b18fb8d0e505966177a652
Nowy <90156768+Nowyme@users.noreply.github.com>
2023-05-11 14:04:40 +0200

    fix

--- Assets/Scripts/PlayerChopping.cs
+++ Assets/Scripts/PlayerChopping.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerChopping : MonoBehaviour {
+public class PlayerChopping : MonoBehaviour
+{
     public float chopRange = 1f;
     public KeyCode chopKey = KeyCode.Space;
     [SerializeField] public GameObject axe; // Reference to the axe GameObject
     public bool canChop = false;
 
     private TreeChopping treeChopping;
+    private GameObject targetObject; // Store the target game object
 
-    void Start() {
+    void Start()
+    {
         treeChopping = FindObjectOfType<TreeChopping>();
         axe.SetActive(false);
     }
@@ -20,21 +23,29 @@ public class PlayerChopping : MonoBehaviour {
         axe.SetActive(true);
     }
 
-    void Update() {
-        if (canChop && Input.GetKeyDown(chopKey)) {
+    void Update()
+    {
+        if (canChop && Input.GetKeyDown(chopKey))
+        {
             ChopTree();
         }
     }
 
-    private void ChopTree() {
+    private void ChopTree()
+    {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, chopRange);
-        foreach (Collider2D collider in colliders) {
+        foreach (Collider2D collider in colliders)
+        {
             TreeChopping tree = collider.GetComponent<TreeChopping>();
-            if (tree != null) {
+            if (tree != null)
+            {
                 tree.Chop();
-                if (axe != null && axe.GetComponent<AxePickup>() != null) {
+                targetObject = collider.gameObject; // Store the target game object
+
+                if (axe != null && axe.GetComponent<AxePickup>() != null)
+                {
                     axe.GetComponent<AxePickup>().SwingAxe(); // Trigger the axe swing animation
-                    axe.GetComponent<AxePickup>().AxeHit(); // Handle axe hits
+                    axe.GetComponent<AxePickup>().AxeHit(targetObject); // Pass the target game object to AxeHit()
                 }
                 break;
             }
